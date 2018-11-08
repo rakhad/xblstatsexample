be8e1cc55ae2198b07bda7a367b9bebd5a2fe37c
Ramsey Khadder <rakhad@microsoft.com>
2018-11-07 18:19:55 -0800

    Init commit

---
+++ Assets/StatHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using Microsoft.Xbox.Services;
+using Microsoft.Xbox.Services.Client;
+
+public class StatHelper : MonoBehaviour
+{
+    internal XboxLiveUser xboxLiveUser;
+
+    public void FlushStatsUsingStatsManager()
+    {
+        if (this.xboxLiveUser == null)
+        {
+            this.xboxLiveUser = SignInManager.Instance.GetPlayer(1);
+        }
+
+        XboxLive.Instance.StatsManager.RequestFlushToService(this.xboxLiveUser, true);
+    }
+
+    public void FlushStatsUsingStatsManagerComponent()
+    {
+        if (this.xboxLiveUser == null)
+        {
+            this.xboxLiveUser = SignInManager.Instance.GetPlayer(1);
+        }
+
+        StatsManagerComponent.Instance.RequestFlushToService(this.xboxLiveUser, true);
+    }
+}
